4cd9c0ef0ae7911176ad5671809dfcaf5cc7870f
Blini Kot <blinikoshki@protonmail.com>
2022-04-22 18:04:03 -0500

    Add main.asm

--- main.asm
+++ main.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eb70257593da06f682a3ddda54a9d260d4fc514f645237f5ca74b08f8da61a6
-size 2
+oid sha256:7a9576e08273d83d936b2f5cc9607972d0e2a411b7d5720b33ac14b03cf113b1
+size 247545787
